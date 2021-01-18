020fbfb94297d4429033e4449e9a65c5b39b788c
Lusia Kristiana Purnama <lusiakpurnama@gmail.com>
2021-01-18 12:33:16 +0700

    Add gulp and styles

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { src, dest, watch, parallel, task, series } = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+const srcDir = 'src';
+const buildDir = 'build';
+
+const files = {
+	allScss: [`${srcDir}/scss/**/*`],
+	scss: [`${srcDir}/scss/*`],
+};
+
+function errorHandler(err) {
+	console.log(err); // eslint-disable-line
+	this.emit('end');
+}
+
+const scssFiles = () => src(files.scss)
+	.pipe(sass({ outputStyle: 'expanded' }).on('error', errorHandler))
+	.pipe(autoprefixer())
+	.pipe(dest(buildDir));
+
+const watchFiles = (done) => {
+	watch(files.allScss, parallel(scssFiles));
+	done();
+};
+
+const clean = () => del([buildDir]);
+
+task(clean);
+task(scssFiles);
+task(
+	'build',
+	parallel(scssFiles),
+);
+task('watch', series('build', watchFiles));
+task('default', series('clean', 'watch'));
