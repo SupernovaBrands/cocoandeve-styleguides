a15654d8b08d2e1f23833d37e620ff98a5990bbf
pebiantara <pebiantara@gmail.com>
2021-01-22 17:47:11 +0700

    [#cpv4y5] blog category menu compounds

---
+++ js/nav-category.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const navCategory = $('.nav-category');
+  if (navCategory.length > 0) {
+    navCategory.find('.link-search').on('click', function () {
+      const searchBox = navCategory.find('.search-box');
+      if (searchBox.length) {
+        searchBox.removeClass('d-none');
+      }
+    });
+    navCategory.find(".search-box__close").on('click', function() {
+      const parent = $(this).parents(".search-box");
+      if (parent.length) {
+        parent.addClass('d-none');
+      }
+    })
+  }
+});
