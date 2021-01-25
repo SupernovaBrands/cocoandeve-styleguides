42afd85981c48f9160caca6f17f7461ce5d7788f
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-01-25 16:06:17 +0700

    [#cpv4vq] section howto with slider video card

--- js/main.js
+++ js/main.js
@@ -37,8 +37,22 @@ $(document).ready(function () {
 	$('#videoCardModal').on('hide.bs.modal', function (e) {
 		$('#videoCardModal video source').attr('src', $videoSrc);
 		$('#videoCardModal video').get(0).pause();
-	})
+	});
 
 
+	//video carousel with neighbors
+	$('.carousel-item', '.show-neighbors').each(function(){
+		var next = $(this).next();
+		if (! next.length) {
+		  next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+	}).each(function(){
+		var prev = $(this).prev();
+		if (! prev.length) {
+		  prev = $(this).siblings(':last');
+		}
+		prev.children(':nth-last-child(2)').clone().prependTo($(this));
+	});
 
 });
