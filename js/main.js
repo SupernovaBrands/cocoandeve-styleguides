0677a90a2c87d59f5af10dedb80e26d5b6568242
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-01-25 21:07:04 +0700

    carousel video mobile

--- js/main.js
+++ js/main.js
@@ -49,18 +49,21 @@ $(document).ready(function () {
 
 
 	//video carousel with neighbors
-	$('.carousel-item', '.show-neighbors').each(function(){
-		var next = $(this).next();
-		if (! next.length) {
-		  next = $(this).siblings(':first');
-		}
-		next.children(':first-child').clone().appendTo($(this));
-	}).each(function(){
-		var prev = $(this).prev();
-		if (! prev.length) {
-		  prev = $(this).siblings(':last');
-		}
-		prev.children(':nth-last-child(2)').clone().prependTo($(this));
-	});
+	if ($('.how-to__show-neighbors').length > 0 && $('.carousel-item').length > 0) {
+		$('.carousel-item', '.how-to__show-neighbors').each(function(){
+			var next = $(this).next();
+			if (! next.length) {
+			  next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+		}).each(function(){
+			var prev = $(this).prev();
+			if (! prev.length) {
+			  prev = $(this).siblings(':last');
+			}
+			prev.children(':nth-last-child(2)').clone().prependTo($(this));
+		});
+	}
+	
 
 });
