37251d0105c7f6a29a8b3557349d84507790bfa8
pebiantara <pebiantara@gmail.com>
2021-01-26 11:37:06 +0700

    revision part 2 updated on breadcrumb social list and category

--- js/nav-category.js
+++ js/nav-category.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
   const navCategory = $('.nav-category');
   if (navCategory.length > 0) {
-    navCategory.find('.link-search').on('click', function () {
-      const searchBox = $('.search-box');
-      if (searchBox.length) {
-        searchBox.removeClass('d-none');
-      }
-    });
-    $(".search-box__close").on('click', function() {
-      const parent = $(this).parents(".search-box");
-      if (parent.length) {
-        parent.addClass('d-none');
-      }
-    })
+    const linkSearch = navCategory.find('.link-search');
+    if (linkSearch) {
+      navCategory.find('.link-search').on('click', function () {
+        const searchBox = navCategory.find('.search-box');
+        if (searchBox.length) {
+          searchBox.removeClass('d-none');
+          $(this).parent().addClass('d-none');
+        }
+      });
+      navCategory.find(".search-box__close").on('click', function() {
+        const parent = $(this).parents(".search-box");
+        if (parent.length) {
+          parent.addClass('d-none');
+          linkSearch.parent().removeClass('d-none');
+        }
+      })
+    }
   }
 });
