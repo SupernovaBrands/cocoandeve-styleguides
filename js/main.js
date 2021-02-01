4ed2fe1e42ac246fa4b9116f7ee13f98e7c64c94
Teddy Maulana <maulanateddy99@gmail.com>
2021-02-01 12:32:45 +0700

    update carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const lg = 992
+
 $(document).ready(function () {
 	window.showGrid = function () {
 		jQuery('body').append('<style type="text/css">.gridoverlay{position:fixed;top:0;left:50%;transform:translateX(-50%);z-index:9999}.gridoverlay .col{height:100vh}.gridoverlay .col:before{content:"";display:block;background-color:rgba(0,123,255,0.3);height:100%}</style><div class="container gridoverlay"><div class="row"><div class="col"></div><div class="col"></div><div class="col"></div><div class="col"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div></div></div>');
@@ -28,10 +30,8 @@ $(document).ready(function () {
 	}
 
 	// Carousel Sidebar
-	const sidebarCarousel = $('#sidebarCarousel')
-	if (sidebarCarousel.length > 0) {
-		if (window.innerWidth < 992) {
-			sidebarCarousel.carousel('cycle')
-		}
+	const sidebarCarousel = $('.carousel--sidebar')
+	if (sidebarCarousel.length > 0 && window.innerWidth < lg) {
+		sidebarCarousel.carousel('cycle')
 	}
 });
