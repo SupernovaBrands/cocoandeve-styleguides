790c290631316565c744f09eeb41fa133bdd0535
pebiantara <pebiantara@gmail.com>
2021-02-01 14:53:23 +0700

    remove rem unit in nav link and update category js

--- js/nav-category.js
+++ js/nav-category.js
@@ -4,17 +4,11 @@ $(document).ready(function () {
     const linkSearch = navCategory.find('.link-search');
     if (linkSearch) {
       navCategory.find('.link-search').on('click', function () {
-        const searchBox = navCategory.find('.search-box');
-        if (searchBox.length) {
-          searchBox.removeClass('d-none');
-        }
+        navCategory.find('.search-box').removeClass('d-none');
       });
       navCategory.find(".search-box__close").on('click', function() {
-        const parent = $(this).parents(".search-box");
-        if (parent.length) {
-          parent.addClass('d-none');
-        }
-      })
+        $(this).parents(".search-box").addClass('d-none');
+      });
     }
   }
 });
