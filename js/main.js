fcc1a1501bd783b7ad2ac90dcebb2526136d672a
Lusia Kristiana Purnama <lusiakpurnama@gmail.com>
2021-02-03 15:13:39 +0700

    sync styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const screenLG = 992
-
 $(document).ready(function () {
+	const screenLG = 992;
+
 	window.showGrid = function () {
 		jQuery('body').append('<style type="text/css">.gridoverlay{position:fixed;top:0;left:50%;transform:translateX(-50%);z-index:9999}.gridoverlay .col{height:100vh}.gridoverlay .col:before{content:"";display:block;background-color:rgba(0,123,255,0.3);height:100%}</style><div class="container gridoverlay"><div class="row"><div class="col"></div><div class="col"></div><div class="col"></div><div class="col"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div><div class="col d-none d-lg-block"></div></div></div>');
 	};
@@ -29,23 +29,34 @@ $(document).ready(function () {
 		});
 	}
 
-	// Carousel Sidebar
-	const sidebarCarousel = $('.carousel--sidebar')
-	if (sidebarCarousel.length > 0 && window.innerWidth < screenLG) {
-		sidebarCarousel.addClass('carousel')
-		sidebarCarousel.carousel('cycle')
+	if ($('.blog__sidebar').length > 0) {
+		const sidebarCarousel = $('.carousel--sidebar');
+		if (sidebarCarousel.length > 0 && window.innerWidth < screenLG) {
+			sidebarCarousel.addClass('carousel');
+			sidebarCarousel.carousel('cycle');
+		}
+
+		$(window).on('resize', function () {
+			if (window.innerWidth >= screenLG) {
+				sidebarCarousel.carousel('dispose');
+				sidebarCarousel.removeClass('carousel');
+			} else {
+				sidebarCarousel.addClass('carousel');
+				sidebarCarousel.carousel('cycle');
+			}
+		});
 	}
 
 	// video modal
 	// Gets the video src from the data-src on each button
-	var $videoSrc;
+	let $videoSrc;
 	if ($('.video-card').length > 0) {
-		$('.video-card picture').on('click',function() {
+		$('.video-card picture').on('click', function () {
 			$videoSrc = $(this).data('src');
 		});
 	}
 
-	$('#videoCardModal').on('shown.bs.modal', function (e) {
+	$('#videoCardModal').on('shown.bs.modal', function () {
 		// set the video src to autoplay and not to show related video.
 		$(this).find('video').find('source').attr('src', $videoSrc);
 		$(this).find('video').get(0).load();
@@ -53,22 +64,23 @@ $(document).ready(function () {
 	});
 
 	// stop playing the youtube video when I close the modal
-	$('#videoCardModal').on('hide.bs.modal', function (e) {
+	$('#videoCardModal').on('hide.bs.modal', function () {
 		$(this).find('video').find('source').attr('src', '');
 		$(this).find('video').get(0).load();
 		$(this).find('video').get(0).pause();
 	});
 
-	//video carousel
+	// video carousel
 	if ($('.carousel--centered').length > 0) {
-		$('.carousel--centered').each( function() {
+		$('.carousel--centered').each(function () {
 			if ($(this).find('.carousel-item').length > 1) {
-
 				$(this).on('slide.bs.carousel', function () {
 					$(this).find('a[data-slide]').removeClass('d-none');
 				});
 
-				var currentIndex, prevSlide, nextSlide;
+				let currentIndex;
+				let prevSlide;
+				let nextSlide;
 				$(this).on('slid.bs.carousel', function () {
 					currentIndex = $(this).find('.carousel-item.active').index();
 					prevSlide = $(this).find('.carousel-item').eq(currentIndex).prev();
@@ -92,5 +104,4 @@ $(document).ready(function () {
 			}
 		});
 	}
-
 });
