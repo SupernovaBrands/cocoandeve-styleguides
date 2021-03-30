bd280b61d9ab5b84f90e6508e11adeff3028d66d
Lusia Kristiana Purnama <lusiakpurnama@gmail.com>
2021-03-30 16:56:39 +0700

    add generated js

---
+++ js/chunk.nav-category.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcocoandeve_styleguides"] = self["webpackChunkcocoandeve_styleguides"] || []).push([["nav-category"],{
+
+/***/ "./scripts/modules/nav-category.js":
+/*!*****************************************!*\
+  !*** ./scripts/modules/nav-category.js ***!
+  \*****************************************/
+/***/ (() => {
+
+eval("$(document).ready(function () {\n  var navCategory = $('.nav-category');\n\n  if (navCategory.length > 0) {\n    var linkSearch = navCategory.find('.link-search');\n\n    if (linkSearch) {\n      linkSearch.on('click', function () {\n        if ($(this).hasClass('opened')) {\n          $(this).removeClass('opened').addClass('text-primary');\n          navCategory.find('.search-box').addClass('d-none');\n        } else {\n          $(this).addClass('opened').removeClass('text-primary');\n          navCategory.find('.search-box').removeClass('d-none');\n        }\n      });\n      navCategory.find('.search-box__close').on('click', function () {\n        linkSearch.removeClass('opened').addClass('text-primary');\n        $(this).parents('.search-box').addClass('d-none');\n      });\n    }\n  }\n});\n\n//# sourceURL=webpack://cocoandeve-styleguides/./scripts/modules/nav-category.js?");
+
+/***/ })
+
+}]);
