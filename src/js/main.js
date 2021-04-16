8b822d2dace81eb3ac2213ec199eadcc87d356eb
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-04-16 15:37:38 +0700

    stockist template

--- src/js/main.js
+++ src/js/main.js
@@ -281,4 +281,15 @@ $(document).ready(function () {
 			$(this).text('Remove');
 		}
 	});
+
+	if ($('.stockist').length > 0) {
+		const stockistPlace = $('.stockist__select').val();
+		$('.stockist figure[data-toggle="' + stockistPlace + '"]').removeClass('d-none');
+		$('.stockist__select').on('change', function () {
+			let selectedPlace = $(this).find('option:selected').text();
+			$('.stockist__location').html(selectedPlace);
+			$('.stockist figure').addClass('d-none');
+			$('.stockist figure[data-toggle="' + $(this).val() + '"]').removeClass('d-none');
+		});
+	}
 });
