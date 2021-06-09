c2410ff2a2311d040bdbc024a304d8a83e4053e9
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-06-09 17:10:46 +0700

    stockist bugfix

--- src/js/main.js
+++ src/js/main.js
@@ -340,12 +340,12 @@ $(document).ready(function () {
 	// stockist
 	if ($('.stockist').length > 0) {
 		const stockistPlace = $('.stockist__select').val();
-		$(`.stockist figure[data-toggle="${stockistPlace}"]`).removeClass('d-none');
+		$(`.stockist figure[data-toggle*="${stockistPlace}"]`).removeClass('d-none');
 		$('.stockist__select').on('change', function () {
 			const selectedPlace = $(this).find('option:selected').text();
 			$('.stockist__location').html(selectedPlace);
 			$('.stockist figure').addClass('d-none');
-			$(`.stockist figure[data-toggle="${$(this).val()}"]`).removeClass('d-none');
+			$(`.stockist figure[data-toggle*="${$(this).val()}"]`).removeClass('d-none');
 		});
 	}
 
@@ -503,4 +503,5 @@ $(document).ready(function () {
 			}
 		});
 	}
+
 });
