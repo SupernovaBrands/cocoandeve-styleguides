4dbef194bc44e183b21b7a3bed7fef8f5f0751e1
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-08-16 11:04:00 +0700

    remove unused

--- src/js/modules/carousel-swipe.js
+++ src/js/modules/carousel-swipe.js
@@ -1,44 +1,42 @@
 $(document).ready(function () {
-	if ($('.carousel--swipe').length > 0) {
-		$('.carousel--swipe').each((index, carousel) => {
-			let xDown = null;
-			let yDown = null;
+	$('.carousel--swipe').each((index, carousel) => {
+		let xDown = null;
+		let yDown = null;
 
-			function getTouches(evt) {
-				return evt.touches || evt.originalEvent.touches;
+		function getTouches(evt) {
+			return evt.touches || evt.originalEvent.touches;
+		}
+
+		function handleTouchStart(evt) {
+			const firstTouch = getTouches(evt)[0];
+			xDown = firstTouch.clientX;
+			yDown = firstTouch.clientY;
+		}
+
+		function handleTouchMove(evt) {
+			if (!xDown || !yDown) {
+				return;
 			}
 
-			function handleTouchStart(evt) {
-				const firstTouch = getTouches(evt)[0];
-				xDown = firstTouch.clientX;
-				yDown = firstTouch.clientY;
-			}
+			const xUp = evt.touches[0].clientX;
+			const yUp = evt.touches[0].clientY;
 
-			function handleTouchMove(evt) {
-				if (!xDown || !yDown) {
-					return;
-				}
+			const xDiff = xDown - xUp;
+			const yDiff = yDown - yUp;
 
-				const xUp = evt.touches[0].clientX;
-				const yUp = evt.touches[0].clientY;
-
-				const xDiff = xDown - xUp;
-				const yDiff = yDown - yUp;
-
-				if (Math.abs(xDiff) > Math.abs(yDiff)) {
-					if (xDiff > 0) {
-						$(carousel).carousel('next');
-					} else {
-						$(carousel).carousel('prev');
-					}
+			if (Math.abs(xDiff) > Math.abs(yDiff)) {
+				if (xDiff > 0) {
+					$(carousel).carousel('next');
 				} else {
-					xDown = null;
-					yDown = null;
+					$(carousel).carousel('prev');
 				}
+			} else {
+				xDown = null;
+				yDown = null;
 			}
+		}
 
-			carousel.addEventListener('touchstart', handleTouchStart, false);
-			carousel.addEventListener('touchmove', handleTouchMove, false);
-		});
-	}
+		carousel.addEventListener('touchstart', handleTouchStart, false);
+		carousel.addEventListener('touchmove', handleTouchMove, false);
+	});
 });
