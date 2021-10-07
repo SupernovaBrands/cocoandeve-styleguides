795c7b20de4a7464bcca8d22f0edf1543ff2f4f7
Pebiantara <pebiantara@gmail.com>
2021-10-07 14:23:01 +0700

    remove multiple shades and keep for subscription

--- src/js/modules/collection-swatch.js
+++ src/js/modules/collection-swatch.js
@@ -1,81 +1,6 @@
 /* global screenLG */
 import { popopOver } from '~mod/utils';
 
-const updateFormButton = (form) => {
-	const selected = form.find('.variant-swatch.border-primary');
-	if (selected.hasClass('waitlist')) {
-		form.parent().find('.collection-swatch__waitlist').removeClass('d-none');
-		form.find('.collection-swatch__price div').removeClass('d-flex').addClass('d-none');
-		form.find('.collection-swatch__price button').addClass('d-none');
-	} else {
-		form.parent().find('.collection-swatch__waitlist').addClass('d-none');
-		form.find('.collection-swatch__price div').removeClass('d-none').addClass('d-flex');
-		form.find('.collection-swatch__price button').removeClass('d-none');
-	}
-
-	if (selected.hasClass('oos')) {
-		form.find('button[type=submit]').text('Out of Stock').attr('disabled', 'disabled');
-	} else {
-		form.find('button[type=submit]').text('Add to Cart').removeAttr('disabled');
-	}
-};
-
-const updateSwatchBundles = function (form, currElem) {
-	const attrPairFor = form.find('.variant-swatch.border-primary');
-	if (attrPairFor.length > 1) {
-		const pairData = [];
-		attrPairFor.each(function (i, el) {
-			pairData.push($(el).data('value'));
-		});
-
-		const dataValue = currElem.attr('data-value');
-		const currentBundle = currElem.data('bundle');
-		let targetElem;
-		if (currentBundle === 'option-2') {
-			targetElem = $('.variant-swatch[data-bundle="option-1"]');
-		} else {
-			targetElem = $('.variant-swatch[data-bundle="option-2"]');
-		}
-
-		if (targetElem.length > 0) {
-			targetElem.each(function (k, elm) {
-				let pairedVal = '';
-				if (currentBundle === 'option-2') {
-					pairedVal = `${$(elm).attr('data-value')}-${dataValue}`;
-				} else {
-					pairedVal = `${dataValue}-${$(elm).attr('data-value')}`;
-				}
-				const varIdElem = $('#swatchBundle').find(`[data-pair=${pairedVal}]`);
-				if (varIdElem.attr('data-available') === 'false') {
-					$(elm).addClass('oos').addClass('waitlist');
-				} else {
-					$(elm).removeClass('oos').removeClass('waitlist');
-				}
-			});
-		}
-	}
-};
-
-$('.collection-swatch, .collection-swatch__modal').on('click', '.variant-swatch', function () {
-	const attrFor = $(this).data('value');
-	const swatchContainers = $(this).closest('form').find('.product-swatch');
-
-	swatchContainers.each((i, el) => {
-		const swatches = $(el).find('.variant-swatch');
-		const selected = swatches.filter(`[data-value=${attrFor}]`);
-		if (selected.length > 0) {
-			swatches.removeClass('border-primary');
-			selected.addClass('border-primary');
-			$(el).find('p').addClass('d-none')
-				.filter(`.swatch-label-${attrFor}`)
-				.removeClass('d-none');
-		}
-	});
-
-	updateSwatchBundles($(this).closest('form'), $(this));
-	updateFormButton($(this).closest('form'));
-});
-
 $('.collection-swatch').on('click', '.collection-swatch__close', function () {
 	$('.collection-swatch').removeClass('show');
 });
@@ -83,14 +8,6 @@ $('.collection-swatch').on('click', '.collection-swatch__close', function () {
 const swatchRender = function (target, content) {
 	if (content !== '') {
 		target.html(content);
-	}
-	if (target.find('.swatch-bundle-variant').length > 0) {
-		target.find('.swatch-bundle-variant').each(function (i, el) {
-			const productForm = $(el).closest('.product-form');
-			updateSwatchBundles(productForm, productForm.find('.variant-swatch.border-primary[data-bundle="option-1"]'));
-			updateSwatchBundles(productForm, productForm.find('.variant-swatch.border-primary[data-bundle="option-2"]'));
-			updateFormButton(productForm);
-		});
 	}
 };
 
