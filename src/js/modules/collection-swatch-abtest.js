d9827ae822091edf2793abb49df0d72a96bb2293
rizkiaprilian <rizki.a.aprilian@gmail.com>
2021-10-27 11:39:31 +0700

    revert for subscription overlay & create suffix

--- src/js/modules/collection-swatch-abtest.js
+++ src/js/modules/collection-swatch-abtest.js
@@ -1,8 +1,6 @@
 /* global screenLG */
-import { popopOver } from '~mod/utils';
-
-$('.collection-swatch').on('click', '.collection-swatch__close', function () {
-	$('.collection-swatch').removeClass('show');
+$('.collection-swatch-abtest').on('click', '.collection-swatch__close', function () {
+	$('.collection-swatch-abtest').removeClass('show');
 });
 
 const swatchRender = function (target, content) {
@@ -11,24 +9,16 @@ const swatchRender = function (target, content) {
 	}
 };
 
-$('.btn-choose__swatch').click(function () {
+$('.btn-choose__swatch-abtest').click(function () {
 	const btn = $(this);
 	if (window.innerWidth < screenLG) {
-		swatchRender($('.collection-swatch'), btn.siblings('.swatch-overlay').html());
-		$('.collection-swatch').addClass('show');
-		$('.collection-swatch > div').addClass('d-none');
-
-		let className = '.collection-swatch--overlay';
-		if ($(btn).hasClass('btn-choose__swatch-subscription')) className = '.collection-swatch--subscription';
-		$(className).removeClass('d-none');
-		popopOver();
-	} else {
-		swatchRender($('.collection-swatch__modal .modal-dialog'), btn.siblings('.swatch-overlay').html());
-		let id = '#collectionSwatchModal';
-		if ($(btn).hasClass('btn-choose__swatch-subscription')) id = '#collectionSubscriptionModal';
-		$(id).modal({
+		swatchRender($('.collection-swatch-abtest'), btn.siblings('.swatch-overlay').html());
+		$('.collection-swatch-abtest').addClass('show');
+		$('.collection-swatch-abtest > div').addClass('d-none');
+	} else if ($('html').hasClass('au-store')) {
+		swatchRender($('.collection-swatch-abtest__modal .modal-dialog'), btn.siblings('.swatch-overlay').html());
+		$('#collectionSwatchModal').modal({
 			show: true,
 		});
-		$(id).on('shown.bs.modal', () => popopOver());
 	}
 });
