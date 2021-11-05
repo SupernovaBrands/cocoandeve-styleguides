e781f38218ae9d6bf4bcaabd5386d957b3c73c7d
Pebiantara <pebiantara@gmail.com>
2021-11-05 16:10:20 +0700

    add prevent default

--- src/js/modules/bfcm-waitlist.js
+++ src/js/modules/bfcm-waitlist.js
@@ -1,5 +1,6 @@
-$('#bfcm-waitlist__form').on('submit', function () {
+$('#bfcm-waitlist__form').on('submit', function (e) {
 	$(this).addClass('d-none');
 	$('.bfcm-waitlist__thank-you').removeClass('d-none');
 	$('.bfcm-waitlist__title').text('You are in!');
+	e.preventDefault();
 });
