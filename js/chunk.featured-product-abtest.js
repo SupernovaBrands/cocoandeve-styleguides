37b6dcc794f827fe4d4c805de35a39534439d2ce
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-12-22 02:57:14 +0000

    Deploying gh-pages from master d1901b9c189e6a462c24289eb388e6d8584b1b1e

--- js/chunk.featured-product-abtest.js
+++ js/chunk.featured-product-abtest.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
@@ -14,7 +15,6 @@
   \*********************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ \"./src/js/modules/utils.js\");\n/* global screenLG */\n\n$('.collection-swatch').on('click', '.collection-swatch__close', function () {\n  $('.collection-swatch').removeClass('show');\n});\n\nvar swatchRender = function swatchRender(target, content) {\n  if (content !== '') {\n    target.html(content);\n  }\n};\n\n$('.btn-choose__swatch').click(function () {\n  var btn = $(this);\n\n  if (window.innerWidth < screenLG) {\n    swatchRender($('.collection-swatch'), btn.siblings('.swatch-overlay').html());\n    $('.collection-swatch').addClass('show');\n    $('.collection-swatch > div').addClass('d-none');\n    var className = '.collection-swatch--overlay';\n    if ($(btn).hasClass('btn-choose__swatch-subscription')) className = '.collection-swatch--subscription';\n    $(className).removeClass('d-none');\n    (0,_utils__WEBPACK_IMPORTED_MODULE_0__.popopOver)();\n  } else {\n    swatchRender($('.collection-swatch__modal .modal-dialog'), btn.siblings('.swatch-overlay').html());\n    var id = '#collectionSwatchModal';\n    if ($(btn).hasClass('btn-choose__swatch-subscription')) id = '#collectionSubscriptionModal';\n    $(id).modal({\n      show: true\n    });\n    $(id).on('shown.bs.modal', function () {\n      return (0,_utils__WEBPACK_IMPORTED_MODULE_0__.popopOver)();\n    });\n  }\n});\n\n//# sourceURL=webpack://cocoandeve-styleguides/./src/js/modules/collection-swatch.js?");
 
 /***/ })
