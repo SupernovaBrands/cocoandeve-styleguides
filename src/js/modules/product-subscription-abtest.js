6c02e7f4e0f16c8a90ec34334b02b92cdfa80b35
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-01-20 15:44:52 +0700

    update js

--- src/js/modules/product-subscription-abtest.js
+++ src/js/modules/product-subscription-abtest.js
@@ -1,16 +1,13 @@
+const subscriptionVariant = $('.product-variant--subscription');
+
 $('#subscriptionCheckbox').on('change', function () {
-	const subscriptionVariant = $('.product-variant--subscription');
+	subscriptionVariant.toggleClass('product-variant--subscription-selected', this.checked);
 	subscriptionVariant.find('input[type="radio"]').prop('checked', true);
-	subscriptionVariant.find('label').toggleClass('bg-white', !this.checked);
-	subscriptionVariant.find('.product-variant__title').toggleClass('d-none', this.checked);
-	subscriptionVariant.find('.product-variant__title-subscription').toggleClass('d-none', !this.checked);
 });
 
 $('input[name="product-variant"]').on('change', function () {
+	subscriptionVariant.removeClass('product-variant--subscription-selected');
 	if (!$(this).closest('.product-variant').hasClass('product-variant--subscription')) {
 		$('#subscriptionCheckbox').prop('checked', false);
-		const subscriptionVariant = $('.product-variant--subscription');
-		subscriptionVariant.find('.product-variant__title').removeClass('d-none');
-		subscriptionVariant.find('.product-variant__title-subscription').addClass('d-none');
 	}
 });
