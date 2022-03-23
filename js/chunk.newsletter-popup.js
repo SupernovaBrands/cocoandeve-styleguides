a37f8477312e2ceff27c330acad7cb118df832a1
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2022-03-23 06:33:03 +0000

    Deploying gh-pages from master d37d4e6993e43d71a4636bb839473b302e55335d

---
+++ js/chunk.newsletter-popup.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcocoandeve_styleguides"] = self["webpackChunkcocoandeve_styleguides"] || []).push([["newsletter-popup"],{
+
+/***/ "./src/js/modules/newsletter-popup.js":
+/*!********************************************!*\
+  !*** ./src/js/modules/newsletter-popup.js ***!
+  \********************************************/
+/***/ (() => {
+
+eval("$('#newsletterModal .newsletter-bigger-popup__form').on('submit', function (e) {\n  e.preventDefault();\n  $('#newsletterModal .newsletter-bigger-popup__form').addClass('d-none');\n  $('#newsletterModal .newsletter-bigger-popup__completed').addClass('d-flex');\n});\n\n//# sourceURL=webpack://cocoandeve-styleguides/./src/js/modules/newsletter-popup.js?");
+
+/***/ })
+
+}]);
