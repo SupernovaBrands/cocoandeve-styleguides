56acfd7537f75268dfb8103a33471ab0523d7609
pebiantara <pebiantara@gmail.com>
2023-07-07 09:56:26 +0700

    [865cnddgg] update product card applicable for mobile only

---
+++ src/js/components/review-star-single.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SvgFull from '~svg/star.svg';
+
+const ReviewStarSingle = () => (
+	<SvgFull role="presentation" className="svg text-primary" />
+);
+
+export default ReviewStarSingle;
