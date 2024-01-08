fe2d995c1d9628f0f889c69fb19ae367057a65f7
Teddy Maulana <maulanateddy99@gmail.com>
2024-01-08 21:39:15 +0700

    general updates for new pdp

--- src/js/modules/carousel-loop.js
+++ src/js/modules/carousel-loop.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
 		});
 	}
 
-	const adjustScrollThumb = (thumb, inner, scrollParent) => {
+	const adjustScrollThumb = (thumb, inner) => {
 		const totalItems = $(inner).find('.carousel-item').length || 1;
 		// eslint-disable-next-line no-param-reassign
 		thumb.style.width = `${((inner.clientWidth / totalItems) / inner.clientWidth) * 100}%`;
@@ -65,22 +65,19 @@ $(document).ready(function () {
 		*/
 	};
 
-	if ($('.carousel--scroll-single').length > 0) {
-		$('.carousel--scroll-single').each((index, carousel) => {
+	if ($('.carousel--scroll-product').length > 0) {
+		$('.carousel--scroll-product').each((index, carousel) => {
+			carousel.classList.remove('carousel--product-preview');
+			const { parentNode } = carousel;
 			const inner = carousel.querySelector('.carousel-inner');
-			const scrollbar = carousel.querySelector('.scrollbar');
-			const scrollThumb = carousel.querySelector('.scrollbar--thumb');
+			const scrollbar = parentNode.querySelector('.scrollbar');
+			const scrollThumb = parentNode.querySelector('.scrollbar--thumb');
 			const totalItems = $(inner).find('.carousel-item').length || 1;
 
 			if (scrollbar) {
 				carousel.addEventListener('adjustThumb', () => { adjustScrollThumb(scrollThumb, inner, scrollbar.parentNode); });
 				if (scrollThumb) adjustScrollThumb(scrollThumb, inner, scrollbar.parentNode);
 			}
-
-			console.log('carousel', carousel, scrollbar);
-
-			let x = 0;
-			let left = 0;
 
 			const scrollbarWidth = scrollbar.clientWidth;
 			const gap = 15;
@@ -89,26 +86,34 @@ $(document).ready(function () {
 			const slideWidth = scrollbarWidth / totalItems;
 			let activeSlide = 0;
 
-			const innerDrag = (e) => {
-				inner.scrollLeft = left - (e.pageX || e.touches[0].pageX) + x;
-				if (scrollThumb) scrollThumb.style.left = `${(inner.scrollLeft / inner.scrollWidth) * 100}%`;
-				// checkButton();
+			const innerDrag = () => {
+				console.log('innerDrag');
 			};
 
 			const scrollDrag = (e) => {
-				console.log('e.touches[0].pageX', e.touches[0].pageX);
+				console.log('scrollDrag');
 				const pageX = (e.pageX || e.touches[0].pageX);
 				let pos = pageX - leftTh;
 				if (pageX < leftTh) pos = 0;
 				else if (pageX > (rightTh - scrollThumb.clientWidth)) pos = scrollbarWidth - scrollThumb.clientWidth;
 				const slidePos = Math.floor(pos / slideWidth);
-				console.log('pos a', pos, activeSlide);
 				if (scrollThumb) scrollThumb.style.left = `${(pos / scrollbarWidth) * 100}%`;
-				if (activeSlide !== slidePos) {
+				if (slidePos !== activeSlide) {
 					activeSlide = slidePos;
-					$(carousel).carousel('next');
+					// $(carousel).carousel(activeSlide);
 				}
-				// checkButton();
+			};
+
+			const scrollReset = (e) => {
+				console.log('reset', activeSlide);
+				if (e.type === 'touchend') {
+					scrollThumb.style.transition = 'left .3s';
+					if (scrollThumb) scrollThumb.style.left = `${((slideWidth * activeSlide) / scrollbarWidth) * 100}%`;
+					$(carousel).carousel(activeSlide);
+					setTimeout(() => { scrollThumb.style.transition = 'left 0s'; }, 50);
+				}
+				document.removeEventListener('touchmove', innerDrag);
+				document.removeEventListener('touchmove', scrollDrag);
 			};
 
 			inner.addEventListener('scroll', () => {
@@ -118,10 +123,6 @@ $(document).ready(function () {
 
 			const eventStart = (e) => {
 				e.preventDefault();
-				x = (e.pageX || e.touches[0].pageX);
-				console.log('event start x', x);
-				left = inner.scrollLeft;
-
 				document.addEventListener(
 					e.type === 'mousedown' ? 'mousemove' : 'touchmove',
 					e.target === scrollThumb ? scrollDrag : innerDrag,
@@ -134,16 +135,20 @@ $(document).ready(function () {
 				scrollThumb.addEventListener('touchstart', eventStart, true);
 				scrollThumb.addEventListener('mousedown', eventStart, true);
 				scrollThumb.addEventListener('touchstart', eventStart, true);
+
+				scrollThumb.addEventListener('touchend', scrollReset);
+				scrollThumb.addEventListener('mouseup', scrollReset);
 			}
 
-			document.addEventListener('mouseup', () => {
-				document.removeEventListener('mousemove', innerDrag);
-				document.removeEventListener('mousemove', scrollDrag);
-			});
-
-			document.addEventListener('touchend', () => {
-				document.removeEventListener('touchmove', innerDrag);
-				document.removeEventListener('touchmove', scrollDrag);
+			$(this).on('slide.bs.carousel', function (e) {
+				setTimeout(() => {
+					const $e = $(e.relatedTarget);
+					const idx = $e.index();
+					console.log(idx);
+					scrollThumb.style.transition = 'left .3s';
+					if (scrollThumb) scrollThumb.style.left = `${((slideWidth * idx) / scrollbarWidth) * 100}%`;
+					setTimeout(() => { scrollThumb.style.transition = 'left 0s'; }, 50);
+				}, 500);
 			});
 		});
 	}
